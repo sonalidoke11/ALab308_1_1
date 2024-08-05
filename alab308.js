f520019b1916e3d2fd43db5bfa1b19a1bae38f9d
sonalidoke11 <sonalidoke810@gmail.com>
2024-08-05 15:34:12 -0400

    Part 2 total time taken calculation

--- alab308.js
+++ alab308.js
@@ -12,7 +12,8 @@ console.log("n1=10 is larger than n4=5" + isFirstLargerThanLast);
 // Check if all numbers are divisible by 5. Cache the result in a variable.
 // Here, we use modulus to check if it is divisible by 5.
 // Since % 5 is 0 it is divisible by 5
-const isDivisibleBy5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) == 0;
+// const isDivisibleBy5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) == 0;
+const isDivisibleBy5 = (n1 % 5) == 0 && (n2 % 5) == 0 && (n3 % 5) == 0 && (n4 % 5) == 0;
 console.log("if all numbers are divisible by 5. Cache the result in a variable = " + isDivisibleBy5);
 
 
@@ -45,3 +46,34 @@ const isValid = isFirstLargerThanLast && isDivisibleBy5 && isUnder25
 
 // Finally, log the results.
 console.log(isValid);
+
+
+
+
+/**
+ * Part 2: Practical Math
+ * var speed = 40;
+    var time = distance/speed*60;
+   return time;
+ */
+
+
+   let totalDistance = 1500;
+
+   let _30milesPerGallon = 30;
+   let _28milesPerGallon = 28;
+   let _23milesPerGallon = 23;
+
+   let _55milesPerHour = 55;
+   let _60milesPerHour = 60;
+   let _75milesPerHour = 75;
+
+   let fuelBudget = 175;
+   let avgFuelCost = 3;
+
+   //var time = distance/speed*60;
+   var totalTime = (1500/55)*60
+   console.log(totalTime);
+
+   var totalHour = totalTime/60;
+   console.log("Total time taken to travel 1500 miles in hour is " + totalHour );
