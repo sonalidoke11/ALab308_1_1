6845736a0d4c1fef62d7d38e79e533c792580662
sonalidoke11 <sonalidoke810@gmail.com>
2024-08-05 14:30:08 -0400

    SImple operations on let and const

---
+++ alab308.js
@@ -0,0 +1,38 @@
+let firstName ="Sonali";  // created variable firstName using let
+let lastName = "P";     // created variable lastName using let
+const age = 34;     // created variable age using const
+
+let counter = 2  // created variable counter using let and set initial value to 2
+const x = true;  // created boolean variable x using const and set initial value to true
+
+console.log(firstName);
+console.log("lastName");
+console.log(age);
+console.log(counter);
+console.log(x);
+
+
+
+// Arithmetic Operations//
+
+let newNumber = 9;
+console.log("Value of variable newNumber = "+newNumber);
+
+newNumber -= 3; // newNumber is now equal to 6
+console.log("Value of newNumber after -=3 = "+newNumber);
+
+newNumber /= 3; // newNumber is now equal to 2
+console.log("Value of newNumber after /=3 = "+newNumber);
+
+newNumber *= 3; // newNumber is now equal to 6
+console.log("Value of newNumber after *=3 = "+newNumber);
+
+newNumber %= 3; // newNumber is now equal to 0
+console.log("Value of newNumber after %=3 = "+newNumber);
+
+let a=9;
+console.log("Value of a = "+a);
+
+let newA = ++a;
+console.log("Value of a = "+newA);
+
